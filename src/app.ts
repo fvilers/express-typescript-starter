5bbafc553384f53007a7b5971d870c31da142c33
Fabian Vilers <fabian.vilers@dev-one.com>
2019-03-18 13:16:40 +0100

    Add compression

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import compression from 'compression';
 import { configuration } from './configuration';
 import { routes } from './routes';
 import { notFound, errorHandler } from './middlewares';
@@ -7,6 +8,8 @@ export const app = express();
 app.set('namespace', 'express-typescript-starter:server');
 app.set('views', configuration.paths.views);
 app.set('view engine', 'hbs');
+
+app.use(compression());
 app.use('/', routes());
 app.use('*', notFound());
 app.use(errorHandler());
