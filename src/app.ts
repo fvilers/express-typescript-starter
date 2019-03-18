867b9a4f5394663b9fcbb8e580844feebcdaa1d2
Fabian Vilers <fabian.vilers@dev-one.com>
2019-03-18 12:45:51 +0100

    Add express

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+app.set('namespace', 'express-typescript-starter:server');
+
+export default app;
---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+export function normalizePort(val: string): string | number | boolean {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
