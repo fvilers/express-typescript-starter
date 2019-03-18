3dd64435cdeb4de75904df896ad4845069420455
Fabian Vilers <fabian.vilers@dev-one.com>
2019-03-18 15:18:14 +0100

    Add error logging in handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,17 +1,20 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import logger from '@fvilers/simple-logger';
 import { isProd } from '../helpers';
 
 export function errorHandler(): ErrorRequestHandler {
   return function(err: any, _req: Request, res: Response, _next: NextFunction) {
+    const { code, message, stack, status } = err;
+
+    logger.error(stack);
+
     if (res.app.get('view engine')) {
-      res.locals.message = err.message;
+      res.locals.message = message;
       res.locals.error = !isProd() ? err : {};
 
-      res.status(err.status || 500);
+      res.status(status || 500);
       res.render('error');
     } else {
-      const { code, message, stack, status } = err;
-
       res.status(status || 500).send({ code, message, stack });
     }
   };
