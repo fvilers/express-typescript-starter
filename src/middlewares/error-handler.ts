f0d1c078b2cf2481e4db295b59680149fd148668
Fabian Vilers <fabian.vilers@dev-one.com>
2019-03-18 13:30:36 +0100

    Improve error handler for no view engine app

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -3,10 +3,16 @@ import { isProd } from '../helpers';
 
 export function errorHandler(): ErrorRequestHandler {
   return function(err: any, _req: Request, res: Response, _next: NextFunction) {
-    res.locals.message = err.message;
-    res.locals.error = !isProd() ? err : {};
+    if (res.app.get('view engine')) {
+      res.locals.message = err.message;
+      res.locals.error = !isProd() ? err : {};
 
-    res.status(err.status || 500);
-    res.render('error');
+      res.status(err.status || 500);
+      res.render('error');
+    } else {
+      const { code, message, stack, status } = err;
+
+      res.status(status || 500).send({ code, message, stack });
+    }
   };
 }
